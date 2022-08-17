75f115c2183098aa270feedc9d38ffe602c4b71a
Yash Gada <gadayash9@gmail.com>
2022-08-17 21:48:58 +0530

    Dealt with collisions

--- HashTable.js
+++ HashTable.js
@@ -4,21 +4,27 @@ function hashStringtoInt(s, tableLength) {
     hash = (13 * hash * s.charCodeAt(i)) % tableLength;
   }
 
-  hash = hash % tableLength;
   console.log(hash);
   return hash;
 }
 class HashTable {
-  table = new Array(11);
+  table = new Array(3);
 
   setItem = (key, value) => {
     const idx = hashStringtoInt(key, this.table.length);
-    this.table[idx] = value;
+    if (!this.table[idx]) {
+      this.table[idx] = [[key, value]];
+    } else if (this.table[idx]) {
+      this.table[idx].push([key, value]);
+    } else {
+      console.log("We have a problem");
+    }
   };
 
   getItem = (key) => {
     const idx = hashStringtoInt(key, this.table.length);
-    return this.table[idx];
+    if (!this.table[idx]) return null;
+    return this.table[idx].find((x) => x[0] === key)[1];
   };
 }
 
@@ -28,5 +34,11 @@ console.log(myTable);
 myTable.setItem("firstName", "Yash");
 console.log(myTable);
 myTable.setItem("lastName", "Gada");
+myTable.setItem("age", "27");
+myTable.setItem("dob", "9/7/1995");
 console.log(myTable);
 console.log(myTable.getItem("firstName"));
+console.log(myTable.getItem("lastName"));
+console.log(myTable.getItem("age"));
+console.log(myTable.getItem("dob"));
+console.log(myTable);
